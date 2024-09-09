5c796eaae03dfe2b02fe97831ca3ea4d449e3c5b
rballentine25 <ballentine.7@wright.edu>
2024-09-09 16:56:43 -0400

    created new file for 24-25 and added arduino code from 23-24

---
+++ 24-25 files/Previous years/arduinoCodes.cpp
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+
+#define DAC_ADDRESS 0x62  // Default I2C address of the MCP4725 DAC
+#define MOTOR_PIN 9       // Example PWM pin connected to the motor controller
+
+
+Adafruit_MCP4725 dac;
+
+
+
+
+void setup() {
+ Serial.begin(9600);
+
+
+ dac.begin(DAC_ADDRESS);
+ dac.setVoltage(0, false);  // Set DAC voltage to 0V initially
+
+
+ pinMode(MOTOR_PIN, OUTPUT);
+
+
+for (int voltage = 0; voltage <= 2000; voltage += 50) {
+   dac.setVoltage(voltage, false);
+   analogWrite(MOTOR_PIN, map(voltage, 0, 2000, 0, 130));
+   delay(50);  // Adjust the delay as needed for your application
+ }
+}
+void loop() {
+
+
+ // If the duration has not been reached, stay at 5V
+ if(int voltage = 2000) {
+   Serial.println("Staying at 4V");
+   Serial.println(voltage);
+   dac.setVoltage(voltage, false);
+   Serial.println(voltage);
+   analogWrite(MOTOR_PIN, 130);
+   Serial.println("Start Delay");
+   delay(60000);
+   Serial.println("Staying at 4V completed");
+
+
+
+
+}
+
+
+}
