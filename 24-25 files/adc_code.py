50fcbd2a3f430ef44021885801a7224ab5038789
rballentine25 <ballentine.7@wright.edu>
2025-02-13 14:06:47 -0500

    updating adc code

--- 24-25 files/adc_code.py
+++ 24-25 files/adc_code.py
@@ -3,6 +3,7 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from time import sleep
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -16,5 +17,7 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P3)
 
-print('Raw ADC Value: ', chan.value)
-print('ADC Voltage: ' + str(chan.voltage) + 'V')
+for i in range(50):
+    print('Raw ADC Value: ', chan.value)
+    print('ADC Voltage: ' + str(chan.voltage) + 'V')
+    sleep(0.25)
