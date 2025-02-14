a2f688aa1a5bdfe89fcd10060cc732909d93a1bf
rballentine25 <ballentine.7@wright.edu>
2025-02-13 19:04:45 -0500

    updating for three probes

--- 24-25 files/reading_ds18b20_probe.py
+++ 24-25 files/reading_ds18b20_probe.py
@@ -7,33 +7,36 @@ import time
 # in the list that matches the device pattern. data is in the file w1_slave within the 
 # device folder
 base_directory = '/sys/bus/w1/devices/'
-dev_folder = glob.glob(base_directory + '28-*')[0]  
-dev_file = dev_folder + '/w1_slave'
+dev_folder_list = glob.glob(base_directory + '28-*')
+temps_farenheit = [0]*3
 
 def read_raw():
-    file = open(dev_file, 'r')
-    data = file.readlines()
-    file.close()
+    num_files = len(dev_folder_list)
+    data = []
+    for i in range(num_files):
+            dev_file = dev_folder_list[i] + '/w1_slave'
+            file = open(dev_file, 'r')
+            data.append(file.readlines())
+            file.close()
     return data
 
 
 def read_temp():
-    data = read_raw()
-    
-    # if the first line is not YES (data read correctly), wait 0.2s and try reading again.
-    # repeat until data is read correctgly
-    while "YES" not in data[0]:
-        time.sleep(0.2)
-        data = read_raw()    
-    
-    # if data is read correctly, look for the t= in the file for the start index, then read 
-    # in the rest of the following string (file ends with the temp)
-    start_index = data[1].find('t=') + 2
-    raw_temp = data[1][start_index:]
-    temp_cels = float(raw_temp) / 1000      # file has temp in "millidegrees"
-    temp_far = temp_cels * (9.0/5.0) + 32.0
-    return [temp_far]
+        data = read_raw()
+        # if the first line is not YES (data read correctly), wait 0.2s and try reading again.
+        # repeat until data is read correctgly
+        for i in range(len(data)):
+            if "YES" not in data[i][0]:
+                continue
+            else:
+                start_index = data[i][1].find('t=') + 2
+                raw_temp = data[i][1][start_index:]
+                temp_cels = float(raw_temp) / 1000      # file has temp in "millidegrees"
+                temp_far = temp_cels * (9.0/5.0) + 32.0
+                temps_farenheit[i] = temp_far
+        
+        return temps_farenheit
 
-for i in range(20):
-    print(read_temp())
-    time.sleep(0.2)
+temps = read_temp()
+for i in range(3):
+    print(temps[i])
