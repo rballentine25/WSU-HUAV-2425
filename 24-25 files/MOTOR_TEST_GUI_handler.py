4e9b3ffbbe4cbc9bb2b339ad7d0dc738954a11a6
rballentine25 <ballentine.7@wright.edu>
2025-02-14 16:56:55 -0500

    need to use pyqtSignal with threading next

--- 24-25 files/MOTOR_TEST_GUI_handler.py
+++ 24-25 files/MOTOR_TEST_GUI_handler.py
@@ -10,11 +10,11 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import threading
 import pandas as pd
+import time
 
 # file definition for the temp probe
 base_directory = '/sys/bus/w1/devices/'
 dev_folder_list = glob.glob(base_directory + '28-*')
-temps_farenheit = [0]*3
 
 # setting up spi for ADC
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -26,6 +26,52 @@ lock = threading.Lock()
 
 # DataFrame to store values of a and b
 df = pd.DataFrame(columns=["time","a", "b", "d"])
+
+# new_voltage = 0.0
+# temps_farenheit = [0]*3
+
+class tempReadingThread(QThread):
+    temps_farenheit = [0]*3
+
+    def read_raw(self):
+        num_files = len(dev_folder_list)
+        data = []
+        for i in range(num_files):
+            dev_file = dev_folder_list[i] + '/w1_slave'
+            with open(dev_file, 'r') as file:
+                data.append(file.readlines())
+        return data
+
+    def run(self):
+        #with lock:
+            while True:
+                data = self.read_raw()
+                # if the first line is not YES (data read correctly), wait 0.2s and try reading again.
+                # repeat until data is read correctgly
+                for i in range(len(data)):
+                    if "YES" in data[i][0]:
+                        start_index = data[i][1].find('t=') + 2
+                        raw_temp = data[i][1][start_index:]
+                        temp_cels = float(raw_temp) / 1000      # file has temp in "millidegrees"
+                        temp_far = temp_cels * (9.0/5.0) + 32.0
+                        self.temps_farenheit[i] = temp_far
+                        #temps_farenheit[i] = temp_cels
+                    else:
+                        continue
+
+                time.sleep(2.5) 
+    
+
+class voltReadingThread(QThread):
+    new_voltage = 0.0
+
+    def run(self):
+        #with lock:
+            while True: 
+                curr_chan = AnalogIn(adc, MCP.P2)
+                new_voltage = curr_chan.voltage
+                time.sleep(.5)
+
 
 class MOTOR_TEST_GUI(QWidget, Ui_Form):
     # INITIALIZATION METHOD
@@ -44,56 +90,34 @@ class MOTOR_TEST_GUI(QWidget, Ui_Form):
         self.ICEEMULATOR.valueChanged.connect(self.update_ice_readout)
         # TEST: self.STARTGEN.valueChanged.connect(self.update_temp_readout)
 
-        # timer
-        self.timer1 = QTimer(self)
-        self.timer2 = QTimer(self)
-        self.timer1.timeout.connect(self.update_temp_readout)
-        self.timer2.timeout.connect(self.update_volt_readout)
-        self.timer2.timeout.connect(self.update_curr_readout)
-        self.timer2.start(300)
-        self.timer1.start(500)
+        # # timer
+        # self.timer1 = QTimer(self)
+        # self.timer2 = QTimer(self)
+        # self.timer1.timeout.connect(self.update_temp_readout)
+        # self.timer2.timeout.connect(self.update_volt_readout)
+        # self.timer2.timeout.connect(self.update_curr_readout)
+        # self.timer2.start(300)
+        # self.timer1.start(500)
 
-        temp_thread = threading.Thread(target=self.update_temp_readout)
-        temp_thread.start()
-        temp_thread.join()
+        # temp_thread = threading.Thread(target=self.update_temp_readout)
+        # temp_thread.start()
+        # temp_thread.join()
 
-        volt_thread = threading.Thread(target=self.update_volt_readout)
-        volt_thread.start()
-        volt_thread.join()
+        # volt_thread = threading.Thread(target=self.update_volt_readout)
+        # volt_thread.start()
+        # volt_thread.join()
+        self.start_sensor_threads()
+  
 
+    def start_sensor_threads(self):
+        self.temp_thread = tempReadingThread()
+        self.volt_thread = voltReadingThread()
 
-
-
-    def read_raw(self):
-        num_files = len(dev_folder_list)
-        data = []
-        for i in range(num_files):
-            dev_file = dev_folder_list[i] + '/w1_slave'
-            with open(dev_file, 'r') as file:
-                data.append(file.readlines())
-            
-        return data
-
-
-    def read_temp(self):
-        global temp_cels
-        data = self.read_raw()
-        # if the first line is not YES (data read correctly), wait 0.2s and try reading again.
-        # repeat until data is read correctgly
-        for i in range(len(data)):
-            if "YES" in data[i][0]:
-                start_index = data[i][1].find('t=') + 2
-                raw_temp = data[i][1][start_index:]
-                temp_cels = float(raw_temp) / 1000      # file has temp in "millidegrees"
-                temp_far = temp_cels * (9.0/5.0) + 32.0
-                #temps_farenheit[i] = temp_far
-                temps_farenheit[i] = temp_cels
-            else:
-                 continue
+        self.temp_thread.temps_farenheit.connect(self.update_temp_readout)
+        self.volt_thread.new_voltage.connect(self.update_volt_readout)
         
-        return temps_farenheit      
-
-
+        self.temp_thread.start()
+        self.volt_thread.start()
 
 # EVENT HANDLERS
     def update_sg_readout(self, newvalue):
@@ -103,22 +127,14 @@ class MOTOR_TEST_GUI(QWidget, Ui_Form):
         self.ICE_READOUT.display(newvalue)
 
     def update_temp_readout(self):
-        global temp_cels
-        # start = time.time()
-        # while True:
-        with lock: 
-            newtemp = self.read_temp()
-
-        self.TEMP_1.display(newtemp[0])
-        self.TEMP_2.display(newtemp[1])
-        self.TEMP_3.display(newtemp[2])
+        #with lock:
+            self.TEMP_1.display(temps_farenheit[0])
+            self.TEMP_2.display(temps_farenheit[1])
+            self.TEMP_3.display(temps_farenheit[2])
 
     def update_volt_readout(self):
-        with lock: 
-            curr_chan = AnalogIn(adc, MCP.P2)
-            new_voltage = curr_chan.voltage
+        # with lock: 
             self.VOLTAGE_READOUT.display(new_voltage)
-
 
     def update_curr_readout(self):
         pass
