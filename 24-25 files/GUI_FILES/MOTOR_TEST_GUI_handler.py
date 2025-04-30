c2fcf40f506e4d1b764783d405ef303044e0d641
rballentine25 <ballentine.7@wright.edu>
2025-04-30 12:01:49 -0400

    final updates to gui scripts and reorganization

--- 24-25 files/GUI_FILES/MOTOR_TEST_GUI_handler.py
+++ 24-25 files/GUI_FILES/MOTOR_TEST_GUI_handler.py
@@ -7,7 +7,7 @@ import busio
 import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+from adafruit_mcp3xxx.analog_in import AnalogIn  
 import threading
 import pandas as pd
 import time
